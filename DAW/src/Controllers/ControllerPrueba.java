749c1e2334e165ab9fa8c8e7070ed29abe7bf88a
Gildartslol <j1amoros944@gmail.com>
2017-05-02 20:30:58 +0200

    Cosas

---
+++ DAW/src/Controllers/ControllerPrueba.java
@@ -0,0 +1,14 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControllerPrueba {
+	@RequestMapping("/text")
+	public ModelAndView text() {
+		MyObject myObject = new MyObject("my name", "my description");
+		return new ModelAndView("text_page").addObject("greetings", "Hello world!").addObject("myobj", myObject);
+	}
+}
---
+++ DAW/src/Controllers/MyObject.java
@@ -0,0 +1,15 @@
+package Controllers;
+public class MyObject {
+	private String name;
+	private String description;
+
+	public MyObject(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public String sayHello() {
+		return "Hello!!!";
+	}
+	// Getters and setters
+}
