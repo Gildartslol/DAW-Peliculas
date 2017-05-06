9215a7402128f73d027cf68e9a31ffd6cff088a2
Gildartslol <j1amoros944@gmail.com>
2017-05-06 21:16:47 +0200

    JDBC

--- DAW/src/DAOS/Conexion.java
+++ DAW/src/DAOS/Conexion.java
@@ -11,7 +11,7 @@ import excepciones.QueryException;
 public class Conexion {
 
 	private static Connection conn;
-	private static final String URL = "jdbc:mysql://localhost:3306/mydb?" + "user=root&password=1212";
+	private static final String URL = "jdbc:mysql://localhost:3306/aplicaciones_web?" + "user=root&password=";
 
 	static {
 		try {
